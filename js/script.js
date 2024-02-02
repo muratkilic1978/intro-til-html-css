f868aa356c54b4f97d020932c46eb71ff8150460
Murat Kilic <muratkilic1978@gmail.com>
2024-02-02 17:04:14 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const video = document.getElementById("myVideo");
+const btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
